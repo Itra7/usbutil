08c1ca9c7467bb8247214ff60edebbda47c90c2a
filip <djurkovicfilip03@gmail.com>
2025-05-26 14:25:02 +0200

    test 2

--- src/usbio.h
+++ src/usbio.h
@@ -67,17 +67,17 @@ struct usbdevfs_conninfo_ex {
 				/* gives at most 6 port entries.           */
 };
 
-#define USBDEVFS_URB_SHORT_NOT_OK	0x01
-#define USBDEVFS_URB_ISO_ASAP		0x02
-#define USBDEVFS_URB_BULK_CONTINUATION	0x04
-#define USBDEVFS_URB_NO_FSBR		0x20	/* Not used */
-#define USBDEVFS_URB_ZERO_PACKET	0x40
-#define USBDEVFS_URB_NO_INTERRUPT	0x80
+#define USBUTIL_USBDEVFS_URB_SHORT_NOT_OK	0x01
+#define USBUTIL_USBDEVFS_URB_ISO_ASAP		0x02
+#define USBUTIL_USBDEVFS_URB_BULK_CONTINUATION	0x04
+#define USBUTIL_USBDEVFS_URB_NO_FSBR		0x20	/* Not used */
+#define USBUTIL_USBDEVFS_URB_ZERO_PACKET	0x40
+#define USBUTIL_USBDEVFS_URB_NO_INTERRUPT	0x80
 
-#define USBDEVFS_URB_TYPE_ISO		   0
-#define USBDEVFS_URB_TYPE_INTERRUPT	   1
-#define USBDEVFS_URB_TYPE_CONTROL	   2
-#define USBDEVFS_URB_TYPE_BULK		   3
+#define USBUTIL_USBDEVFS_URB_TYPE_ISO		   0
+#define USBUTIL_USBDEVFS_URB_TYPE_INTERRUPT	   1
+#define USBUTIL_USBDEVFS_URB_TYPE_CONTROL	   2
+#define USBUTIL_USBDEVFS_URB_TYPE_BULK		   3
 
 struct usbdevfs_iso_packet_desc {
 	unsigned int length;
@@ -121,22 +121,22 @@ struct usbdevfs_hub_portinfo {
 };
 
 /* System and bus capability flags */
-#define USBDEVFS_CAP_ZERO_PACKET		0x01
-#define USBDEVFS_CAP_BULK_CONTINUATION		0x02
-#define USBDEVFS_CAP_NO_PACKET_SIZE_LIM		0x04
-#define USBDEVFS_CAP_BULK_SCATTER_GATHER	0x08
-#define USBDEVFS_CAP_REAP_AFTER_DISCONNECT	0x10
-#define USBDEVFS_CAP_MMAP			0x20
-#define USBDEVFS_CAP_DROP_PRIVILEGES		0x40
-#define USBDEVFS_CAP_CONNINFO_EX		0x80
-#define USBDEVFS_CAP_SUSPEND			0x100
+#define USBUTIL_USBDEVFS_CAP_ZERO_PACKET		0x01
+#define USBUTIL_USBDEVFS_CAP_BULK_CONTINUATION		0x02
+#define USBUTIL_USBDEVFS_CAP_NO_PACKET_SIZE_LIM		0x04
+#define USBUTIL_USBDEVFS_CAP_BULK_SCATTER_GATHER	0x08
+#define USBUTIL_USBDEVFS_CAP_REAP_AFTER_DISCONNECT	0x10
+#define USBUTIL_USBDEVFS_CAP_MMAP			0x20
+#define USBUTIL_USBDEVFS_CAP_DROP_PRIVILEGES		0x40
+#define USBUTIL_USBDEVFS_CAP_CONNINFO_EX		0x80
+#define USBUTIL_USBDEVFS_CAP_SUSPEND			0x100
 
 /* USBDEVFS_DISCONNECT_CLAIM flags & struct */
 
 /* disconnect-and-claim if the driver matches the driver field */
-#define USBDEVFS_DISCONNECT_CLAIM_IF_DRIVER	0x01
+#define USBUTIL_USBDEVFS_DISCONNECT_CLAIM_IF_DRIVER	0x01
 /* disconnect-and-claim except when the driver matches the driver field */
-#define USBDEVFS_DISCONNECT_CLAIM_EXCEPT_DRIVER	0x02
+#define USBUTIL_USBDEVFS_DISCONNECT_CLAIM_EXCEPT_DRIVER	0x02
 
 struct usbdevfs_disconnect_claim {
 	unsigned int interface;
@@ -155,48 +155,48 @@ struct usbdevfs_streams {
  * linux/usb/ch9.h
  */
 
-#define USBDEVFS_CONTROL           _IOWR('U', 0, struct usbdevfs_ctrltransfer)
-#define USBDEVFS_CONTROL32           _IOWR('U', 0, struct usbdevfs_ctrltransfer32)
-#define USBDEVFS_BULK              _IOWR('U', 2, struct usbdevfs_bulktransfer)
-#define USBDEVFS_BULK32              _IOWR('U', 2, struct usbdevfs_bulktransfer32)
-#define USBDEVFS_RESETEP           _IOR('U', 3, unsigned int)
-#define USBDEVFS_SETINTERFACE      _IOR('U', 4, struct usbdevfs_setinterface)
-#define USBDEVFS_SETCONFIGURATION  _IOR('U', 5, unsigned int)
-#define USBDEVFS_GETDRIVER         _IOW('U', 8, struct usbdevfs_getdriver)
-#define USBDEVFS_SUBMITURB         _IOR('U', 10, struct usbdevfs_urb)
-#define USBDEVFS_SUBMITURB32       _IOR('U', 10, struct usbdevfs_urb32)
-#define USBDEVFS_DISCARDURB        _IO('U', 11)
-#define USBDEVFS_REAPURB           _IOW('U', 12, void *)
-#define USBDEVFS_REAPURB32         _IOW('U', 12, __u32)
-#define USBDEVFS_REAPURBNDELAY     _IOW('U', 13, void *)
-#define USBDEVFS_REAPURBNDELAY32   _IOW('U', 13, __u32)
-#define USBDEVFS_DISCSIGNAL        _IOR('U', 14, struct usbdevfs_disconnectsignal)
-#define USBDEVFS_DISCSIGNAL32      _IOR('U', 14, struct usbdevfs_disconnectsignal32)
-#define USBDEVFS_CLAIMINTERFACE    _IOR('U', 15, unsigned int)
-#define USBDEVFS_RELEASEINTERFACE  _IOR('U', 16, unsigned int)
-#define USBDEVFS_CONNECTINFO       _IOW('U', 17, struct usbdevfs_connectinfo)
-#define USBDEVFS_IOCTL             _IOWR('U', 18, struct usbdevfs_ioctl)
-#define USBDEVFS_IOCTL32           _IOWR('U', 18, struct usbdevfs_ioctl32)
-#define USBDEVFS_HUB_PORTINFO      _IOR('U', 19, struct usbdevfs_hub_portinfo)
-#define USBDEVFS_RESET             _IO('U', 20)
-#define USBDEVFS_CLEAR_HALT        _IOR('U', 21, unsigned int)
-#define USBDEVFS_DISCONNECT        _IO('U', 22)
-#define USBDEVFS_CONNECT           _IO('U', 23)
-#define USBDEVFS_CLAIM_PORT        _IOR('U', 24, unsigned int)
-#define USBDEVFS_RELEASE_PORT      _IOR('U', 25, unsigned int)
-#define USBDEVFS_GET_CAPABILITIES  _IOR('U', 26, __u32)
-#define USBDEVFS_DISCONNECT_CLAIM  _IOR('U', 27, struct usbdevfs_disconnect_claim)
-#define USBDEVFS_ALLOC_STREAMS     _IOR('U', 28, struct usbdevfs_streams)
-#define USBDEVFS_FREE_STREAMS      _IOR('U', 29, struct usbdevfs_streams)
-#define USBDEVFS_DROP_PRIVILEGES   _IOW('U', 30, __u32)
-#define USBDEVFS_GET_SPEED         _IO('U', 31)
+#define USBUTIL_USBDEVFS_CONTROL           _IOWR('U', 0, struct usbdevfs_ctrltransfer)
+#define USBUTIL_USBDEVFS_CONTROL32           _IOWR('U', 0, struct usbdevfs_ctrltransfer32)
+#define USBUTIL_USBDEVFS_BULK              _IOWR('U', 2, struct usbdevfs_bulktransfer)
+#define USBUTIL_USBDEVFS_BULK32              _IOWR('U', 2, struct usbdevfs_bulktransfer32)
+#define USBUTIL_USBDEVFS_RESETEP           _IOR('U', 3, unsigned int)
+#define USBUTIL_USBDEVFS_SETINTERFACE      _IOR('U', 4, struct usbdevfs_setinterface)
+#define USBUTIL_USBDEVFS_SETCONFIGURATION  _IOR('U', 5, unsigned int)
+#define USBUTIL_USBDEVFS_GETDRIVER         _IOW('U', 8, struct usbdevfs_getdriver)
+#define USBUTIL_USBDEVFS_SUBMITURB         _IOR('U', 10, struct usbdevfs_urb)
+#define USBUTIL_USBDEVFS_SUBMITURB32       _IOR('U', 10, struct usbdevfs_urb32)
+#define USBUTIL_USBDEVFS_DISCARDURB        _IO('U', 11)
+#define USBUTIL_USBDEVFS_REAPURB           _IOW('U', 12, void *)
+#define USBUTIL_USBDEVFS_REAPURB32         _IOW('U', 12, __u32)
+#define USBUTIL_USBDEVFS_REAPURBNDELAY     _IOW('U', 13, void *)
+#define USBUTIL_USBDEVFS_REAPURBNDELAY32   _IOW('U', 13, __u32)
+#define USBUTIL_USBDEVFS_DISCSIGNAL        _IOR('U', 14, struct usbdevfs_disconnectsignal)
+#define USBUTIL_USBDEVFS_DISCSIGNAL32      _IOR('U', 14, struct usbdevfs_disconnectsignal32)
+#define USBUTIL_USBDEVFS_CLAIMINTERFACE    _IOR('U', 15, unsigned int)
+#define USBUTIL_USBDEVFS_RELEASEINTERFACE  _IOR('U', 16, unsigned int)
+#define USBUTIL_USBDEVFS_CONNECTINFO       _IOW('U', 17, struct usbdevfs_connectinfo)
+#define USBUTIL_USBDEVFS_IOCTL             _IOWR('U', 18, struct usbdevfs_ioctl)
+#define USBUTIL_USBDEVFS_IOCTL32           _IOWR('U', 18, struct usbdevfs_ioctl32)
+#define USBUTIL_USBDEVFS_HUB_PORTINFO      _IOR('U', 19, struct usbdevfs_hub_portinfo)
+#define USBUTIL_USBDEVFS_RESET             _IO('U', 20)
+#define USBUTIL_USBDEVFS_CLEAR_HALT        _IOR('U', 21, unsigned int)
+#define USBUTIL_USBDEVFS_DISCONNECT        _IO('U', 22)
+#define USBUTIL_USBDEVFS_CONNECT           _IO('U', 23)
+#define USBUTIL_USBDEVFS_CLAIM_PORT        _IOR('U', 24, unsigned int)
+#define USBUTIL_USBDEVFS_RELEASE_PORT      _IOR('U', 25, unsigned int)
+#define USBUTIL_USBDEVFS_GET_CAPABILITIES  _IOR('U', 26, __u32)
+#define USBUTIL_USBDEVFS_DISCONNECT_CLAIM  _IOR('U', 27, struct usbdevfs_disconnect_claim)
+#define USBUTIL_USBDEVFS_ALLOC_STREAMS     _IOR('U', 28, struct usbdevfs_streams)
+#define USBUTIL_USBDEVFS_FREE_STREAMS      _IOR('U', 29, struct usbdevfs_streams)
+#define USBUTIL_USBDEVFS_DROP_PRIVILEGES   _IOW('U', 30, __u32)
+#define USBUTIL_USBDEVFS_GET_SPEED         _IO('U', 31)
 /*
  * Returns struct usbdevfs_conninfo_ex; length is variable to allow
  * extending size of the data returned.
  */
-#define USBDEVFS_CONNINFO_EX(len)  _IOC(_IOC_READ, 'U', 32, len)
-#define USBDEVFS_FORBID_SUSPEND    _IO('U', 33)
-#define USBDEVFS_ALLOW_SUSPEND     _IO('U', 34)
-#define USBDEVFS_WAIT_FOR_RESUME   _IO('U', 35)
+#define USBUTIL_USBDEVFS_CONNINFO_EX(len)  _IOC(_IOC_READ, 'U', 32, len)
+#define USBUTIL_USBDEVFS_FORBID_SUSPEND    _IO('U', 33)
+#define USBUTIL_USBDEVFS_ALLOW_SUSPEND     _IO('U', 34)
+#define USBUTIL_USBDEVFS_WAIT_FOR_RESUME   _IO('U', 35)
 
-#endif /* _UAPI_LINUX_USBDEVICE_FS_H */
+#endif 
